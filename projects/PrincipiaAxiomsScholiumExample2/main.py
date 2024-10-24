bfde4ad3e0c804efc1a84f48b07a337616cece10
gallusline <95877381+gallusline@users.noreply.github.com>
2024-10-24 17:41:30 -0500

    Work on Illst 2

--- projects/PrincipiaAxiomsScholiumExample2/main.py
+++ projects/PrincipiaAxiomsScholiumExample2/main.py
@@ -5,6 +5,7 @@ class TwoSwingingBalls(Scene):
         document here
     '''
     def construct(self):
+        
         ax = Axes(
             x_range=[-1,10,1],
             y_range=[-1,10,1],
@@ -16,51 +17,51 @@ class TwoSwingingBalls(Scene):
         labels.set_color(BLACK)
         self.add(ax, labels)
 
-        E_Text = Text('E').scale(0.75)
-        E_Text.set_color(BLACK)
-        self.play(
-            Write(E_Text)
-        )
-        self.play(E_Text.animate.shift(LEFT*4))
-        self.play(E_Text.animate.shift(UP*2))
+        dot_E  = Dot(ax.coords_to_point(1,7), color=RED)
+        dot_G  = Dot(ax.coords_to_point(2,6.7), color=BLUE)
+        dot_F  = Dot(ax.coords_to_point(7,7), color=RED)
+        dot_H  = Dot(ax.coords_to_point(8,6.7), color=BLUE)
 
-        G_Text = Text('G').scale(0.75)
-        G_Text.set_color(BLACK)
-        self.play(
-            Write(G_Text)
-        )
-        self.play(G_Text.animate.shift(LEFT*3))
-        self.play(G_Text.animate.shift(UP*2))
+        self.add(dot_E,dot_G, dot_F,dot_H)
 
-        F_Text = Text('F').scale(0.75)
-        F_Text.set_color(BLACK)
-        self.play(
-            Write(F_Text)
-        )
-        self.play(F_Text.animate.shift(RIGHT*3))
-        self.play(F_Text.animate.shift(UP*2))
+        E_Text    = Text('E').scale(0.5).next_to(dot_E, LEFT*0.3)
+        E_Text.set_color(RED)
+        self.play(Write(E_Text))
+        F_Text    = Text('F').scale(0.5).next_to(dot_F, RIGHT*0.3)
+        F_Text.set_color(RED)
+        self.play(Write(F_Text))
+        dots_EF   = VGroup(dot_E, dot_F)
 
-        H_Text = Text('H').scale(0.75)
-        H_Text.set_color(BLACK)
-        self.play(
-            Write(H_Text)
-        )
-        self.play(H_Text.animate.shift(RIGHT*4))
-        self.play(H_Text.animate.shift(UP*2))
+        G_Text = Text('G').scale(0.5).next_to(dot_G, LEFT*0.3)
+        G_Text.set_color(BLUE)
+        self.play(Write(G_Text))
+        H_Text = Text('H').scale(0.5).next_to(dot_H, RIGHT*0.3)
+        H_Text.set_color(BLUE)
+        self.play( Write(H_Text))
+        dots_GH = VGroup(dot_G, dot_H)
 
-        '''C_Text = Text('C').scale(0.75)
-        C_Text.set_color(BLACK)
-        self.play(
-            Write(C_Text)
-        )
-        self.play(C_Text.animate.shift(LEFT))
-        
-        D_Text = Text('D').scale(0.75)
-        D_Text.set_color(BLACK)
-        self.play(
-            Write(D_Text)
-        )
-        self.play(D_Text.animate.shift(RIGHT*0.5))
-        '''
+
+        line_EF    = Line()
+        line_EF.put_start_and_end_on(dot_E.get_center(),dot_F.get_center())
+        line_EF.set_color(RED)
+        self.play(Write(line_EF))
+
+        line_GH    = Line()
+        line_GH.put_start_and_end_on(dot_G.get_center(),dot_H.get_center())
+        line_GH.set_color(BLUE)
+        self.play(Write(line_GH))
+
+        dot_C  = Dot(ax.coords_to_point(4.5,7), color=RED)
+        dot_D  = Dot(ax.coords_to_point(5.5,6.7), color=BLUE)
+        self.add(dot_C, dot_D)
+
+        C_Text = Text('C').scale(0.5).next_to(dot_C, UP*0.3)
+        C_Text.set_color(RED)
+        self.play(Write(C_Text))
+
+        D_Text = Text('D').scale(0.5).next_to(dot_D, UP*0.45)
+        D_Text.set_color(BLUE)
+        self.play(Write(D_Text))
+        arc= ArcBetweenPoints(start=2 * RIGHT, end=2 * UP, stroke_color=YELLOW)
 
         self.wait(2)
